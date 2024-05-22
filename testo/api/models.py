3b2d3259556f2889f62bfa5ecd2e5ddf9c05f5ef
markos787 <149382126+markos787@users.noreply.github.com>
2024-05-22 20:48:05 +0200

    wip: data types

--- testo/api/models.py
+++ testo/api/models.py
@@ -1,27 +1,38 @@
 from django.db import models
 from django.contrib.gis.db import models as models_gis
+import numpy as np
 
 # Create your models here.
 class zasadzki(models.Model):
-    gid=models.TextField()
-    typ=models.TextField()
-    ranking=models.TextField()
+    gid=models.IntegerField()
+    typ=models.IntegerField()
+    ranking=models.FloatField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if isinstance(self.ranking, str):
+            self.ranking = float(self.ranking.replace(',', '.'))
 
     def __str__(self):
-        return self.title
+        return f'{self.gid} - {self.typ} - {self.ranking:.2f}'
     
     class Meta:
         managed=False
         db_table='zasadzki'
 
 class Location(models.Model):
-    gid=models.TextField()
-    typ=models.TextField()
-    ranking=models.TextField()
+    gid=models.IntegerField()
+    typ=models.IntegerField()
+    ranking=models.FloatField()
     geom=models_gis.PolygonField()
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if isinstance(self.ranking, str):
+            self.ranking = float(self.ranking.replace(',', '.'))    
+
     def __str__(self):
-        return self.title
+        return f'{self.gid} - {self.typ} - {self.ranking:.2f}'
     
     class Meta:
         managed=False
--- testo/api/serializers.py
+++ testo/api/serializers.py
@@ -2,13 +2,21 @@ from rest_framework import serializers
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
 from .models import zasadzki, Location
 
-class zasadzkiSerializer (serializers.ModelSerializer):
+class zasadzkiSerializer(serializers.ModelSerializer):
+    ranking = serializers.SerializerMethodField()
     class Meta:
         model=zasadzki
         fields='__all__'
 
+    def get_ranking(self, obj):
+        return f"{obj.ranking:.2f}"
+
 class LocationSerializer(GeoFeatureModelSerializer):
+    ranking = serializers.SerializerMethodField()
     class Meta:
         model=Location
         geo_field='geom'
         fields='__all__'
+
+    def get_ranking(self, obj):
+        return f"{obj.ranking:.2f}"
